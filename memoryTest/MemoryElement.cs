bad3863aa9c4764c0eecb1564d3d9cb7c8c4eb57
Nikita Dolmatov <SpektrEG@gmail.com>
2015-11-24 12:08:20 +0300

    Add some tests

---
+++ memoryTest/MemoryElement.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nsMemoryTest
+{
+    class MemoryElement
+    {
+        static long elementsInMemory = 0;
+        public static long ElementsInMemory
+        {
+            get { return MemoryElement.elementsInMemory; }
+            set { MemoryElement.elementsInMemory = value; }
+        }
+
+        static long allElementsInMemory = 0;
+        public static long AllElementsInMemory
+        {
+            get { return MemoryElement.allElementsInMemory; }
+            set { MemoryElement.allElementsInMemory = value; }
+        }
+
+        long nElement;
+        public long NElement
+        {
+            get { return nElement; }
+            set { nElement = value; }
+        }
+
+        public MemoryElement()
+        {
+            this.NElement = ++AllElementsInMemory;
+            ElementsInMemory++;
+        }
+
+        ~MemoryElement()
+        {
+            Console.WriteLine("{0,5} is deleted. {1,5}\\{2,5} in memory.", this.NElement, --ElementsInMemory, AllElementsInMemory);
+        }
+    }
+}
---
+++ memoryTest/MemoryFiringField.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nsMemoryTest
+{
+    class MemoryFiringField
+    {
+        const int round = 1000;
+        long all;
+        public long All
+        {
+            get { return all; }
+            set { all = value; }
+        }
+
+        public void Go()
+        {
+            ConsoleKeyInfo cki;
+            int step;
+            while (true)
+            {
+                Console.WriteLine();
+                Console.Write("For continue - press any key, for exit - ctrl+x:");
+                cki = Console.ReadKey(true);
+                if (cki.Key == ConsoleKey.X && cki.Modifiers == ConsoleModifiers.Control)
+                {
+                    break;
+                }
+                for(step = 0; step < round; step++)
+                {
+                    Console.Write("\r{0}", step);
+                    new MemoryElement();
+                }
+                Console.WriteLine();
+                All += step;
+
+                Console.WriteLine(All);
+            }
+        }
+    }
+}
